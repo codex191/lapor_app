b6729ef8a460308e56405c3885ef463cbe8a1e42
Brian Agustian K <agustianbrian25@gmail.com>
2022-09-24 08:31:01 +0700

    Add styles into the project

---
+++ lib/syles.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0XFFFCD8D4);
+const Color secondaryColor = Color.fromARGB(255, 8, 21, 198);
+const Color buttonColor = Color.fromARGB(255, 216, 7, 7);
+const Color bgColor = Color(0XFFFAFAFA);
