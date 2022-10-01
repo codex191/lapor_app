696c59843ef477b56fe3d9e7494dc718b17cb3f3
Brian Agustian K <agustianbrian25@gmail.com>
2022-10-01 15:31:33 +0700

    Menambahkan widget dan menambahkan page baru

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lapor_app/widget/menu_tile.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,8 +8,25 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Lapor App'),
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.blueAccent,
+        elevation: 4,
+        leading: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Image.network(
+              'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/original/commons/new-ui-logo.png'),
+        ),
+      ),
+      endDrawer: Drawer(
+        child: SafeArea(
+          child: Column(
+            children: const <Widget>[
+              MenuTile(title: 'Academy'),
+              MenuTile(title: 'Challenge'),
+              MenuTile(title: 'Event'),
+            ],
+          ),
+        ),
       ),
       body: const Center(
         child: Text("Test Haja"),
--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:lapor_app/ui/home_page.dart';
+import 'package:lapor_app/widget/menu_tile.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -98,7 +99,7 @@ class _LoginPageState extends State<LoginPage> {
               const SizedBox(height: 16),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                  primary: Colors.blueGrey.shade800,
+                  primary: Colors.blueAccent,
                 ),
                 onPressed: () {
                   Navigator.push(context, MaterialPageRoute(builder: (context) {
@@ -192,32 +193,6 @@ class _LoginPageState extends State<LoginPage> {
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class MenuTile extends StatelessWidget {
-  final String title;
-
-  const MenuTile({
-    Key? key,
-    required this.title,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(
-        title,
-        style: Theme.of(context)
-            .textTheme
-            .bodyText1!
-            .copyWith(fontWeight: FontWeight.bold),
-      ),
-      trailing: const Icon(
-        Icons.navigate_next,
-        color: Colors.black,
       ),
     );
   }
