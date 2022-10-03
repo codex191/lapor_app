0dfd7565d2164ae9c1f4f6f55cbaf4cd73b0f1d8
Brian Agustian K <agustianbrian25@gmail.com>
2022-10-03 09:16:08 +0700

    Update Saja

---
+++ lib/ui/aduan_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AduanPage extends StatefulWidget {
+  const AduanPage({super.key});
+
+  @override
+  State<AduanPage> createState() => _AduanPageState();
+}
+
+class _AduanPageState extends State<AduanPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
