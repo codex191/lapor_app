2768dafd9de75ffd20695847a745164bd781254b
Brian Agustian K <agustianbrian25@gmail.com>
2022-10-07 16:59:56 +0700

    Menambahkan skrip program

---
+++ lib/ui/admin/home_page_admin.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePageAdmin extends StatefulWidget {
+  const HomePageAdmin({super.key});
+
+  @override
+  State<HomePageAdmin> createState() => _HomePageAdminState();
+}
+
+class _HomePageAdminState extends State<HomePageAdmin> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
